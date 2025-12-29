cdeb0cde23fdc9e29976912d9235fab755f9a5e2
Ridwan Muse <ridwan.muse.java25@edu.edugrade.se>
2025-12-29 13:37:49 +0100

    update

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 x= 6
 print(x)
-bismillah
+
+print("-----------------")
+
+age = 66
+
+if age < 18:
+    print("you are minor")
+elif age < 65:
+    print(" you are adult")
+
+else:
+    print("you are senior")
+
+    print("------------------")
+
+    
